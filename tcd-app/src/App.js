d0954354d81d1f76859fea6e5ef918728aba9627
Jaime Santos Alcón <jsantosalcon@gmail.com>
2020-05-03 20:01:37 -0400

    Updated text

--- tcd-app/src/App.js
+++ tcd-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Elastic Container Service Lab</h2>
+        <h2>Elastic Container Service Lab: version 21</h2>
       </header>
     </div>
   );
