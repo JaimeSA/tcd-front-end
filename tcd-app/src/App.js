ac98422849a757cb00ca218bba6309df1854ceb9
Jaime Santos Alcón <jsantosalcon@gmail.com>
2020-05-06 17:24:56 -0400

    I just implemented a new feature

--- tcd-app/src/App.js
+++ tcd-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Elastic Container Service Lab: version 27</h2>
+        <h2>Elastic Container Service Lab: HELLO GUYS!!!</h2>
       </header>
     </div>
   );
